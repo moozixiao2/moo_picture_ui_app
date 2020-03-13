c644dcc0190e2e643eec9aa93e13fb5ccc1a0610
Zioo Moo <571869136@qq.com>
2020-03-13 19:03:59 +0800

    分段器使用

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,13 +1,94 @@
 <template>
-  <view>home</view>
+  <view>
+    <view class="home_tab">
+      <view class="home_tab_title">
+        <view class="title_inner">
+          <uni-segmented-control
+            :current="current"
+            :values="items.map(v => v.title)"
+            @clickItem="onClickItem"
+            style-type="text"
+            active-color="#d21974"
+          ></uni-segmented-control>
+        </view>
+        <view class="iconfont iconsearch"></view>
+      </view>
+      <view class="home_tab_content">
+        <view v-if="current === 0">
+          <home-recommend />
+        </view>
+        <view v-if="current === 1">
+          <home-category />
+        </view>
+        <view v-if="current === 2">
+          <home-new />
+        </view>
+        <view v-if="current === 3">
+          <home-album />
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script>
-export default {
+import homeAlbum from "./home-album";
+import homeCategory from "./home-category";
+import homeNew from "./home-new";
+import homeRecommend from "./home-recommend";
 
-}
+import { uniSegmentedControl } from "@dcloudio/uni-ui";
+export default {
+  components: {
+    homeAlbum,
+    homeCategory,
+    homeNew,
+    homeRecommend,
+    uniSegmentedControl
+  },
+  data() {
+    return {
+      items: [
+        {
+          title: "推荐"
+        },
+        {
+          title: "分类"
+        },
+        {
+          title: "最新"
+        },
+        {
+          title: "专辑"
+        }
+      ],
+      current: 0
+    };
+  },
+  methods: {
+    onClickItem(e) {
+      if (this.current !== e.currentIndex) {
+        this.current = e.currentIndex;
+      }
+    }
+  }
+};
 </script>
 
-<style>
-
+<style lang="scss">
+.home_tab{
+  .home_tab_title{
+    position: relative;
+    .title_inner{
+      width: 60%;
+      margin: 0 auto;
+    }
+    .iconsearch {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+      right: 5%;
+    }
+  }
+}
 </style>
